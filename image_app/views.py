df6e9763e17041c7a2c8c0f15bfac1af6e35b500
kayofujii <kayosamu1025@gmail.com>
2022-02-11 16:32:28 +0900

    opencvで顔認識ができるようにした

--- image_app/views.py
+++ image_app/views.py
@@ -29,7 +29,8 @@ def edit_image(request, id):
     params = {}
     uploaded_image = UploadedImage.objects.get(id=id)
     if "button_gray" in request.POST:
-        change_gray(uploaded_image)
+        recognize_face(uploaded_image)
+        # change_gray(uploaded_image)
         uploaded_image.edit_image = get_image_path(uploaded_image)
         print(get_image_path(uploaded_image))
         uploaded_image.save()
@@ -55,3 +56,30 @@ def get_image_path(before_im):
             (before_im.name + before_im.image.url).encode("utf-8")).hexdigest(),
         os.path.splitext(before_im.image.url)[1],
     )
+
+
+def recognize_face(uploaded_image):
+    # https://note.nkmk.me/image-processing/
+    # 参考:https://note.nkmk.me/python-opencv-face-detection-haar-cascade/
+    face_cascade = cv2.CascadeClassifier(
+        settings.OPENCV_PATH + '/data/haarcascades/haarcascade_frontalface_default.xml')
+    print(settings.OPENCV_PATH +
+          '/data/haarcascades/haarcascade_frontalface_default.xml')
+
+    url = uploaded_image.image.url
+    path = str(settings.BASE_DIR) + url
+    print(path)
+    src = cv2.imread(path)
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(src_gray)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        face = src[y: y + h, x: x + w]
+        face_gray = src_gray[y: y + h, x: x + w]
+
+    output = str(settings.BASE_DIR) + '/media' + \
+        get_image_path(uploaded_image)
+
+    cv2.imwrite(output, src)
