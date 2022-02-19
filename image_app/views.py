1d7395a2648de832626b0cf64fc5edfbff5cac9d
kayofujii <kayosamu1025@gmail.com>
2022-02-19 20:57:03 +0900

    一旦終了

--- image_app/views.py
+++ image_app/views.py
@@ -88,54 +88,12 @@ def recognize_face(uploaded_image):
     path = str(settings.BASE_DIR) + url
     src = cv2.imread(path)
 
-    # 背景を透過する
-    # https://qiita.com/AtomJamesScott/items/ccef87b1092d7407de0d
-    img = cv2.imread(path)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
-    edges = cv2.dilate(edges, None)
-    edges = cv2.erode(edges, None)
-    contour_info = []
-    contours, _ = cv2.findContours(
-        edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    for c in contours:
-        contour_info.append((
-            c,
-            cv2.isContourConvex(c),
-            cv2.contourArea(c),
-        ))
-    contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
-    max_contour = contour_info[0]
-    mask = np.zeros(edges.shape)
-    cv2.fillConvexPoly(mask, max_contour[0], (255))
-
-    mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
-    mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
-    mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
-
-    mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
-
-    mask_stack = mask_stack.astype(
-        'float32') / 255.0          # Use float matrices,
-    img = img.astype('float32') / 255.0  # for easy blending
-
-    masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR)  # Blend
-    # Convert back to 8-bit
-    masked = (masked * 255).astype('uint8')
-
-    c_red, c_green, c_blue = cv2.split(img)
-
-    # merge with mask got on one of a previous steps
-    img_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
-
-    cv2.imwrite(str(settings.BASE_DIR) +
-                get_tmp_image_path(uploaded_image, 'crop2'), img_a*255)
-
     faces = face_cascade.detectMultiScale(src)
+    face = sorted(faces, reverse=True, key=lambda x: x[2])[0]
 
-    for x, y, w, h in [faces[0]]:
-        face = src[y-15:y+h+30, x-15:x+w+30]
+    x, y, w, h = face[0], face[1], face[2], face[3]
+    sab = int(w*0.25)
+    face = src[y-sab:y+h+sab, x-sab:x+w+sab]
     im_rgba = face.copy()
 
     # opencv→pillow変換 https://qiita.com/derodero24/items/f22c22b22451609908ee
@@ -145,7 +103,7 @@ def recognize_face(uploaded_image):
     im_a = Image.new("L", im_rgba.size, 0)
     draw = ImageDraw.Draw(im_a)
     draw.ellipse((0, 0, im_rgba.size[0], im_rgba.size[0]), fill=255)
-    im_a = im_a.filter(ImageFilter.GaussianBlur(4))
+    im_a = im_a.filter(ImageFilter.GaussianBlur(10))
 
     # 丸に顔をいれる
     im_rgba.putalpha(im_a)
@@ -155,24 +113,18 @@ def recognize_face(uploaded_image):
         get_tmp_image_path(uploaded_image, 'crop')
     im_rgba_crop.save(crop_path)
 
+    # 商品画像の顔を識別
     pro_path = str(settings.BASE_DIR) + uploaded_image.product_im.url
-    pro_im = Image.open(pro_path)
-    copy_pro_im = pro_im.copy()
-
     pro_src = cv2.imread(pro_path)
     pro_faces = face_cascade.detectMultiScale(pro_src)
+    pro_face = sorted(pro_faces, reverse=True, key=lambda x: x[2])[0]
 
-    for x, y, w, h in [pro_faces[0]]:
-        pro_face = src[y-15:y+h+30, x-15:x+w+30]
-        pro_x = x
-        pro_y = y
-        pro_w = w
-        pro_h = h
-    size_sabun = int(pro_w*0.5)
-    ichi_sabun = int(pro_x*0.1)
+    px, py, pw, ph = pro_face[0], pro_face[1], pro_face[2], pro_face[3]
 
-    im_rgba_crop = im_rgba_crop.resize((pro_w+size_sabun, pro_h+size_sabun))
-    copy_pro_im.paste(im_rgba_crop, (pro_x-ichi_sabun, pro_y-ichi_sabun),
+    pro_im = Image.open(pro_path)
+    copy_pro_im = pro_im.copy()
+    im_rgba_crop = im_rgba_crop.resize((pw+sab*2, ph+sab*2))
+    copy_pro_im.paste(im_rgba_crop, (px-sab, py-sab),
                       im_rgba_crop.split()[3])
     copy_pro_im.save(str(settings.BASE_DIR) +
                      get_tmp_image_path(uploaded_image, 'paste'))
